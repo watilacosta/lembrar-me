2e326f6ac94912aae7e0a19885ba41b2266409f4
Watila Costa <watilafreire26@gmail.com>
2025-03-02 13:17:18 -0300

    template added

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,15 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+if (document.getElementById('nav-mobile-btn')) {
+    document.getElementById('nav-mobile-btn').addEventListener('click', function () {
+        if (this.classList.contains('close')) {
+            document.getElementById('nav').classList.add('hidden');
+            this.classList.remove('close');
+        } else {
+            document.getElementById('nav').classList.remove('hidden');
+            this.classList.add('close');
+        }
+    });
+}
